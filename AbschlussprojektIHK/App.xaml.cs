ed39e524e374fb6e3bc3d97747e6c097157a3cef
felwanew <47968469+felwanew@users.noreply.github.com>
2021-04-18 12:17:36 +0200

    config files

--- AbschlussprojektIHK/App.xaml.cs
+++ AbschlussprojektIHK/App.xaml.cs
@@ -13,5 +13,35 @@ namespace AbschlussprojektIHK
     /// </summary>
     public partial class App : Application
     {
+        
+        public IServiceProvider ServiceProvider { get; private set; }
+
+    public IConfiguration Configuration { get; private set; }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var builder = new ConfigurationBuilder()
+         .SetBasePath(Directory.GetCurrentDirectory())
+         .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        Configuration = builder.Build();
+
+        Console.WriteLine(Configuration.GetConnectionString("BloggingDatabase"));
+
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+
+        var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        // ...
+
+        services.AddTransient(typeof(MainWindow));
+    }
     }
 }
